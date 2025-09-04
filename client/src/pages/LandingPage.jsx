f4ead13716751d776009ecd056058bd8a30f5f5b
rshdgvn <rasheedgavinesponga@gmail.com>
2025-09-05 00:51:28 +0800

    apply helper function to handle logged in user

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,14 +1,19 @@
-import { Button } from "@/components/ui/button"
-import { useNavigate } from "react-router"
+import { Button } from "@/components/ui/button";
+import { isLoggedIn } from "@/lib/auth";
+import { useNavigate } from "react-router";
 
 function LandingPage() {
   const nav = useNavigate();
 
-  return (
+  return isLoggedIn() ? (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button onClick={() => nav('/login')}>Sign in</Button>
+      <Button onClick={() => nav("/dashboard")}>Dashboard</Button>
     </div>
-  )
+  ) : (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button onClick={() => nav("/login")}>Sign in</Button>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;

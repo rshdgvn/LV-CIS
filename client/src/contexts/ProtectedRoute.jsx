27ad5489fe055046daf72bed4fbd3aef2b380e32
rshdgvn <rasheedgavinesponga@gmail.com>
2025-10-23 22:53:53 +0800

    fix unauthorized access

--- client/src/contexts/ProtectedRoute.jsx
+++ client/src/contexts/ProtectedRoute.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { useAuth } from "./AuthContext";
 import { Outlet } from "react-router-dom";
+import Forbidden from "@/pages/errors/Forbidden";
 
 const ProtectedRoute = () => {
   const { user } = useAuth();
 
-  if (user) {
+  if (user.id != null && user.email != "") {
     return <Outlet />;
   }
+
+  return <Forbidden />;
 };
 
 export default ProtectedRoute;

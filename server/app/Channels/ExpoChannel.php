45d53ef4c0c0dad501b0cc6f06e9fb626fde9f02
rshdgvn <rasheedgavinesponga@gmail.com>
2026-04-26 22:30:11 +0800

    fix: not sending notif

--- server/app/Channels/ExpoChannel.php
+++ server/app/Channels/ExpoChannel.php
@@ -4,30 +4,40 @@ namespace App\Channels;
 
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ExpoChannel
 {
-    public function send($notifiable, Notification $notification)
-    {
-        if (!method_exists($notification, 'toExpo')) {
-            return;
-        }
-
-        $message = $notification->toExpo($notifiable);
-
-        if (!$notifiable->expo_push_token || empty($message)) {
-            return;
-        }
-
-        Http::post('https://exp.host/--/api/v2/push/send', [
-            'to' => $notifiable->expo_push_token,
-            'title' => $message['title'],
-            'body' => $message['body'],
-            'sound' => 'default',
-            'data' => [
-                'url' => '/profile/notifications',
-                'message' => $message['data']
-            ]
-        ]);
+  public function send($notifiable, Notification $notification)
+  {
+    if (!method_exists($notification, 'toExpo')) {
+      return;
     }
+
+    $message = $notification->toExpo($notifiable);
+
+    if (!$notifiable->expo_push_token || empty($message)) {
+      \Log::warning('ExpoChannel: skipped', [
+        'token' => $notifiable->expo_push_token,
+        'message_empty' => empty($message)
+      ]);
+      return;
+    }
+
+    $response = Http::post('https://exp.host/--/api/v2/push/send', [
+      'to' => $notifiable->expo_push_token,
+      'title' => $message['title'],
+      'body' => $message['body'],
+      'sound' => 'default',
+      'data' => [
+        'url' => '/profile/notifications',
+        ...$message['data']
+      ]
+    ]);
+
+    Log::info('ExpoChannel response', [
+      'status' => $response->status(),
+      'body' => $response->json()
+    ]);
+  }
 }
